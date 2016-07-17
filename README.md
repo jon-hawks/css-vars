41e30d1d71f326d304a3f7fb9db1c66fbb37513e
Jon Hawks <conall.hawks@gmail.com>
2016-07-17 05:19:50 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# CSS variables polyfill; enables support for CSS variables on older browsers.
+	By: Jon Hawks
+
+## Usage:
+	<script async defer src="https://path/to/gitcdn"></script>
---
+++ css-vars.js
@@ -0,0 +1,40 @@
+// CSS variables polyfill; enables support for CSS variables on older browsers.
+// Author: Jon Hawks
+
+// Page load events for IE and everyone else.
+document.addEventListener('readystatechange', function(){ if(document.readyState === 'complete') cssVars() });
+window.onload = cssVars();
+
+function cssVars(){
+	if(typeof window.CSS !== 'function' || typeof window.CSS.supports !== 'function' || !window.CSS.supports('(--property: value)')){
+		// Get all of the stylesheets.
+		var styleElements = document.getElementsByTagName('style');
+		
+		// Find CSS variables.
+		var foundVars = {};
+		for(var i = styleElements.length - 1; i >= 0; i--){
+			if(typeof styleElements[i].innerHTML !== 'string') continue;
+			foundVars[i] = styleElements[i].innerHTML.match(/(--.+:.+;)/g);
+		}
+		
+		// Parse CSS variables.
+		var parsedVars = {};
+		for(var i in foundVars){
+			if(foundVars[i] !== 'object' && !foundVars[i]) continue;
+			for(var j = foundVars[i].length - 1; j >= 0; j--){
+				var curVar = foundVars[i][j].split(/:\s*/);
+				parsedVars[curVar[0]] = curVar[1].replace(/;/, '');
+			}
+		}
+		
+		// Replace CSS variables.
+		for(var i in styleElements){
+			if(typeof styleElements[i].innerHTML === 'undefined') continue;
+			for(var j in parsedVars){
+				var regex = new RegExp('var\\(\\s*' + j + '\\s*\\)', 'g');
+				styleElements[i].innerHTML = styleElements[i].innerHTML.replace(regex, parsedVars[j]);
+				var regex2 = new RegExp('var\\(\\s*.+\\s*,\\s*(.+)\\)', 'g');
+			}
+		}
+	}
+}
